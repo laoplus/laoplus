c74c9b583d2aa98c346e363b0059ff21d84dcc12
eai04191 <eai04191@users.noreply.github.com>
2021-11-26 21:44:48 +0000

    update with 9e7c8c1f9a9de65a01eb438e51c8ee2fd24f200c

---
+++ laoplus.user.js
@@ -0,0 +1,27 @@
+
+// ==UserScript==
+// @name        LAOPLUS
+// @namespace   net.mizle
+// @version     0.0.0
+// @author      Irie Aoi <eai@mizle.net>
+// @description ブラウザ版ラストオリジンのプレイを支援する userscript
+// @homepageURL https://github.com/eai04191/laoplus
+// @supportURL  https://github.com/eai04191/laoplus/issues
+// @run-at      document-idle
+// @match       https://pc-play.games.dmm.co.jp/play/lastorigin_r/*
+// @match       https://pc-play.games.dmm.com/play/lastorigin/*
+// @match       https://osapi.dmm.com/gadgets/ifr?synd=dmm&container=dmm&owner=*&viewer=*&aid=616121&*
+// @match       https://osapi.dmm.com/gadgets/ifr?synd=dmm&container=dmm&owner=*&viewer=*&aid=699297&*
+// @match       https://adult-client.last-origin.com/
+// @match       https://normal-client.last-origin.com/
+// @require     https://cdn.jsdelivr.net/npm/lodash@4.17.21/lodash.min.js
+// @require     https://unpkg.com/micromodal/dist/micromodal.min.js
+// @require     https://cdn-tailwindcss.vercel.app
+// ==/UserScript==
+
+(function () {
+	'use strict';
+
+	console.log("Hello!");
+
+})();
