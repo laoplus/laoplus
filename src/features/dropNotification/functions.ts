8cf72c4ace33975cda5838851bfff1257fe89563
Irie Aoi <eai@mizle.net>
2021-12-06 00:27:40 +0900

    /wave_clearの型を適当に追加
    PcDropNotificationはCreatePCInfosからPCRewardListを使うように変更

--- src/features/dropNotification/functions.ts
+++ src/features/dropNotification/functions.ts
@@ -7,45 +7,59 @@ import {
     sendToDiscordWebhook,
 } from "../discordNotification";
 
-type CreatePCInfo = {
-    PCId: number;
-    Index: string;
+type WaveClearResponse = {
+    ClearRewardInfo: ClearRewardInfo;
+};
+
+type ClearRewardInfo = {
+    PCRewardList: RewardPC[];
+    ItemRewardList: RewardItem[];
+};
+type RewardPC = {
     Grade: number;
     Level: number;
+    PCKeyString: string;
+};
+type RewardItem = {
+    ItemKeyString: string;
 };
 
 /**
  * @package
  */
-export const PcDropNotification = (res: { CreatePCInfos: CreatePCInfo[] }) => {
-    const embeds = res.CreatePCInfos.reduce((embeds: Embed[], pc) => {
-        // ランクB, Aを無視
-        if (pc.Grade === 2 || pc.Grade === 3) return embeds;
+export const PcDropNotification = (res: WaveClearResponse) => {
+    const embeds = res.ClearRewardInfo.PCRewardList.reduce(
+        (embeds: Embed[], pc) => {
+            // ランクB, Aを無視
+            if (pc.Grade === 2 || pc.Grade === 3) return embeds;
 
-        const id = pc.Index.replace(/^Char_/, "").replace(/_N$/, "");
-        const name = unsafeWindow.LAOPLUS.tacticsManual.locale[`UNIT_${id}`];
-        const rank = gradeToRank(pc.Grade);
+            const id = pc.PCKeyString.replace(/^Char_/, "").replace(/_N$/, "");
+            const name =
+                unsafeWindow.LAOPLUS.tacticsManual.locale[`UNIT_${id}`];
+            const rank = gradeToRank(pc.Grade);
 
-        // クラゲ
-        if (id.startsWith("Core")) return embeds;
+            // クラゲ
+            if (id.startsWith("Core")) return embeds;
 
-        // 強化モジュール
-        if (id.startsWith("Module")) return embeds;
+            // 強化モジュール
+            if (id.startsWith("Module")) return embeds;
 
-        embeds.push({
-            title: name || id,
-            color:
-                rank !== ""
-                    ? colorHexToInteger(rankColor[rank].hex())
-                    : undefined,
-            url: `https://lo.swaytwig.com/units/${id}`,
-            thumbnail: {
-                url: `https://lo.swaytwig.com/assets/webp/tbar/TbarIcon_${id}_N.webp`,
-            },
-        });
+            embeds.push({
+                title: name || id,
+                color:
+                    rank !== ""
+                        ? colorHexToInteger(rankColor[rank].hex())
+                        : undefined,
+                url: `https://lo.swaytwig.com/units/${id}`,
+                thumbnail: {
+                    url: `https://lo.swaytwig.com/assets/webp/tbar/TbarIcon_${id}_N.webp`,
+                },
+            });
 
-        return embeds;
-    }, []);
+            return embeds;
+        },
+        []
+    );
 
     const body = { embeds };
 
