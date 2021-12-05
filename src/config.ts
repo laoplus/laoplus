4f3f5ad344efdf140c152869ae182134767197fe
Irie Aoi <eai@mizle.net>
2021-12-06 00:04:15 +0900

    ホイールスクロール増幅機能実装

--- src/config.ts
+++ src/config.ts
@@ -15,6 +15,10 @@ const defaultConfig = {
                 exploration: true,
             },
         },
+        wheelAmplify: {
+            enabled: true,
+            ratio: 10,
+        },
     },
 };
 
