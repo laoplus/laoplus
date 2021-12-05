74ad6433c3d9faedd9dc685c925c71c1e4592a4d
eai04191 <eai04191@users.noreply.github.com>
2021-12-05 13:28:53 +0000

    update with 9ebe2428c5af136dae9f58a546e2095267435fdc

--- laoplus-develop.user.js
+++ laoplus-develop.user.js
@@ -2,7 +2,7 @@
 // ==UserScript==
 // @name        LAOPLUS-DEVELOP
 // @namespace   net.mizle
-// @version     0.1.0-8921f03cecf890743d08ec7f02b6dfd34844e554
+// @version     0.1.0-9ebe2428c5af136dae9f58a546e2095267435fdc
 // @author      Eai <eai@mizle.net>
 // @description ブラウザ版ラストオリジンのプレイを支援する Userscript
 // @homepageURL https://github.com/eai04191/laoplus
@@ -713,6 +713,91 @@
         },
     };
 
+    const isInputElement = (target) => {
+        if (target === null)
+            return false;
+        const t = target;
+        if (t.tagName !== "INPUT")
+            return false;
+        return t;
+    };
+    const getCursorPosition = (element) => {
+        const cursorPosition = element.selectionStart;
+        if (cursorPosition === null) {
+            throw new Error("cursor position should not be null");
+        }
+        return cursorPosition;
+    };
+    // https://stackoverflow.com/questions/23892547/what-is-the-best-way-to-trigger-onchange-event-in-react-js#46012210
+    const getNativeInputValueSetter = () => {
+        const nativeInputValueSetter = Object.getOwnPropertyDescriptor(window.HTMLInputElement.prototype, "value")?.set;
+        if (!nativeInputValueSetter) {
+            throw new Error("nativeInputValueSetter is not found");
+        }
+        return nativeInputValueSetter;
+    };
+    const dispatchEvent = (input, newValue) => {
+        const nativeInputValueSetter = getNativeInputValueSetter();
+        nativeInputValueSetter.call(input, newValue);
+        const inputEvent = new Event("input", { bubbles: true });
+        input.dispatchEvent(inputEvent);
+    };
+    const keypressObserver = () => {
+        // prettier-ignore
+        // addEventListenerで改行されるとネストが深くなるため
+        unsafeWindow.addEventListener("keypress", ({ key, target: eventTraget }) => {
+            const target = isInputElement(eventTraget);
+            if (!target)
+                return;
+            // dispatchした時点でカーソルの位置が吹っ飛んでしまうのでここで抑えておく
+            const cursorPosition = getCursorPosition(target);
+            const lastValue = target.value;
+            const newValue = [
+                ...[...lastValue].slice(0, cursorPosition),
+                key,
+                ...[...lastValue].slice(cursorPosition),
+            ].join("");
+            dispatchEvent(target, newValue);
+            target.setSelectionRange(cursorPosition + 1, cursorPosition + 1);
+        });
+    };
+    const keydownObserver = () => {
+        unsafeWindow.addEventListener("keydown", ({ key, target: eventTraget }) => {
+            const target = isInputElement(eventTraget);
+            if (!target)
+                return;
+            if (!(key === "ArrowRight" ||
+                key === "ArrowLeft" ||
+                key === "Backspace")) {
+                return;
+            }
+            const cursorPosition = getCursorPosition(target);
+            let newCursorPosition;
+            if (key === "Backspace") {
+                const lastValue = target.value;
+                const newValue = [
+                    ...[...lastValue].slice(0, cursorPosition - 1),
+                    ...[...lastValue].slice(cursorPosition),
+                ].join("");
+                dispatchEvent(target, newValue);
+            }
+            if (key === "ArrowRight") {
+                newCursorPosition = cursorPosition + 1;
+            }
+            else {
+                // Backspace, ArrowLeftで共通
+                // 左端で左を押したとき、0未満にならないようにする
+                newCursorPosition =
+                    cursorPosition - 1 >= 0 ? cursorPosition - 1 : cursorPosition;
+            }
+            target.setSelectionRange(newCursorPosition, newCursorPosition);
+        });
+    };
+    const initInputObserver = () => {
+        keypressObserver();
+        keydownObserver();
+    };
+
     // 'return' outside of functionでビルドがコケるのを防ぐ即時実行関数
     (function () {
         const isGameWindow = injection();
@@ -736,6 +821,7 @@
         initUi();
         initInterceptor();
         initResizeObserver();
+        initInputObserver();
         initTacticsManual();
     })();
 
