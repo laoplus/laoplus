c3e2a318a2f8053387bbf3bdf4943ea66034bf88
Irie Aoi <eai@mizle.net>
2021-12-07 16:07:53 +0900

    mitt追加

--- src/banner.js
+++ src/banner.js
@@ -26,6 +26,7 @@
 // @require     https://unpkg.com/dayjs@1.10.7/plugin/relativeTime.js
 // @require     https://unpkg.com/dayjs@1.10.7/plugin/isSameOrBefore.js
 // @require     https://unpkg.com/dayjs@1.10.7/plugin/duration.js
+// @require     https://unpkg.com/mitt@3.0.0/dist/mitt.umd.js
 // @resource    TacticsManualIcon https://lo.swaytwig.com/assets/icon.png
 // ==/UserScript==
 
