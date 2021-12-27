1e52ec344da08f6dd971f613d06b3a17f4561cdc
Irie Aoi <eai@mizle.net>
2021-12-27 20:41:11 +0900

    不要なIDを削除

--- src/ui/components/ConfigModal/FeatureSectionSummary.tsx
+++ src/ui/components/ConfigModal/FeatureSectionSummary.tsx
@@ -19,7 +19,6 @@ export const FeatureSectionSummary: React.VFC<{
             <div className="absolute left-0 top-0 flex items-center justify-center -ml-10 w-10 h-full">
                 <input
                     type="checkbox"
-                    id="laoplus-discord-notification"
                     className="after:content-[''] after:absolute after:inset-0 w-4 h-4 after:cursor-pointer"
                     {...register}
                 />
