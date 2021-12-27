640e7800a2ff518a887d54be5820e7865e4cf18c
Irie Aoi <eai@mizle.net>
2021-12-28 01:30:54 +0900

    シェブロンを開くとき下、閉じるとき上に変更
    - 今の状態ではなく押したらどうなるかを表す
    - トランジション削除

--- src/ui/components/ConfigModal/FeatureSectionSummary.tsx
+++ src/ui/components/ConfigModal/FeatureSectionSummary.tsx
@@ -13,8 +13,8 @@ export const FeatureSectionSummary: React.VFC<{
                 {helpLink && <HelpIcon href={helpLink} />}
             </h2>
 
-            <div className="details-chevron flex items-center transition-transform">
-                <i className="bi bi-chevron-left"></i>
+            <div className="details-chevron flex items-center">
+                <i className="bi bi-chevron-down"></i>
             </div>
             <label className="absolute left-0 top-0 flex items-center justify-center -ml-10 w-10 h-full cursor-pointer">
                 <input
