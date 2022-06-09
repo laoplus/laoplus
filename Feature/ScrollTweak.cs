244485b5c6590971e54f4fab52829addac19ab02
Irie Aoi <eai@mizle.net>
2022-06-10 01:20:58 +0900

    ScrollTweakをUIScrollViewに対応させる

--- Feature/ScrollTweak.cs
+++ Feature/ScrollTweak.cs
@@ -1,3 +1,4 @@
+using System;
 using HarmonyLib;
 
 namespace LAOPLUS.Feature
@@ -5,15 +6,50 @@ namespace LAOPLUS.Feature
     /// <summary>
     /// ホイールスクロールに関連する機能
     /// </summary>
-    [HarmonyPatch(typeof(UIReuseScrollView), nameof(UIReuseScrollView.Scroll))]
+    [HarmonyPatch]
     public static class ScrollTweak
     {
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(UIReuseScrollView), nameof(UIReuseScrollView.Scroll))]
+        [HarmonyPatch(typeof(UIScrollView), nameof(UIScrollView.Scroll))]
         static void Prefix(ref float delta)
         {
             delta = delta * LAOPLUS.ConfigScrollPatchMultiplier.Value;
             if (LAOPLUS.ConfigVerboseLogging.Value)
             {
                 LAOPLUS.Log.LogInfo($"ScrollPatch: {delta}");
+            }
+        }
+
+        [HarmonyPatch(
+            typeof(UIReuseScrollView),
+            nameof(UIReuseScrollView.RestrictWithinBounds),
+            new[] { typeof(bool), typeof(bool), typeof(bool) }
+        )]
+        public static class ScrollingOoBPatchForUIReuseScrollView
+        {
+            static void Prefix(UIReuseScrollView __instance)
+            {
+                if (LAOPLUS.ConfigDisableScrollingOoB.Value)
+                {
+                    __instance.dragEffect = UIReuseScrollView.DragEffect.Momentum;
+                }
+            }
+        }
+
+        [HarmonyPatch(
+            typeof(UIScrollView),
+            nameof(UIScrollView.RestrictWithinBounds),
+            new[] { typeof(bool), typeof(bool), typeof(bool) }
+        )]
+        public static class ScrollingOoBPatchForUIScrollView
+        {
+            static void Prefix(UIScrollView __instance)
+            {
+                if (LAOPLUS.ConfigDisableScrollingOoB.Value)
+                {
+                    __instance.dragEffect = UIScrollView.DragEffect.Momentum;
+                }
             }
         }
     }
