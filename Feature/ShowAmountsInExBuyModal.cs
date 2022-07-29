a2b07519c63fc4cc13ee2024190a6d04c7a87cbe
Irie Aoi <eai@mizle.net>
2022-07-29 13:50:30 +0900

    Item・ExBuyModalも対応

---
+++ Feature/ShowAmountsInExBuyModal.cs
@@ -0,0 +1,61 @@
+using HarmonyLib;
+
+namespace LAOPLUS.Feature
+{
+    [HarmonyPatch(typeof(Panel_ExShopBuyMsg), nameof(Panel_ExShopBuyMsg.SetExShopData))]
+    public class ShowAmountsInExBuyModal
+    {
+        public static void Postfix(Panel_ExShopBuyMsg __instance)
+        {
+            var shopData = __instance._exShopData;
+            LAOPLUS.Log.LogInfo($"ShowAmountsInExBuyModal.Postfix: {shopData.SellItemKeyString}");
+
+            switch (shopData.SellItemKeyString)
+            {
+                // Equipで始まるものだと「設備研究施設品」が引っかかる
+                case { } s
+                    when s.StartsWith("Equip_Chip_")
+                        || s.StartsWith("Equip_Sub_")
+                        || s.StartsWith("Equip_System_"):
+                {
+                    var items = SingleTon<DataManager>.Instance._invenItemInfo;
+                    var amounts = 0;
+                    foreach (var item in items)
+                    {
+                        if (item.ItemKeyString == shopData.SellItemKeyString)
+                        {
+                            amounts++;
+                        }
+                    }
+
+                    __instance._lblItemDesc.text =
+                        $"（{amounts}個所持）\n" + __instance._lblItemDesc.text;
+                    return;
+                }
+                case "Normal_Module":
+                case "Advanced_Module":
+                case "Special_Module":
+                case { } s
+                    when s.StartsWith("Consumable_")
+                        || s.StartsWith("Ev_Consumable_")
+                        || s.StartsWith("CharTicket_Char_Core_") // 代用コア
+                        || s.StartsWith("Favor_MaxExpand_Lv1")
+                        || s.StartsWith("TestItem_") // 急速完成回路・リンク解除器など
+                        || s.StartsWith("Favor_") // ケーキ・花束
+                        || s.StartsWith("RobotParts_")
+                        || s.EndsWith("Parts_T1")
+                        || s.EndsWith("Parts_T2")
+                        || s.EndsWith("Parts_T3"):
+                {
+                    var amounts = SingleTon<DataManager>.Instance.GetItemConsumableStackCount(
+                        shopData.SellItemKeyString
+                    );
+
+                    __instance._lblItemDesc.text =
+                        $"（{amounts}個所持）\n" + __instance._lblItemDesc.text;
+                    return;
+                }
+            }
+        }
+    }
+}
--- Feature/ShowAmountsInItemTooltip.cs
+++ Feature/ShowAmountsInItemTooltip.cs
@@ -1,20 +1,46 @@
 using HarmonyLib;
 
+using Il2CppList = Il2CppSystem.Collections.Generic.List<ClientItemInfo>;
+
 namespace LAOPLUS.Feature
 {
-    [HarmonyPatch(
-        typeof(Panel_ItemTooltip),
-        nameof(Panel_ItemTooltip.SetItemTooltip),
-        typeof(Table_ItemConsumable)
-    )]
+    [HarmonyPatch(typeof(Panel_ItemTooltip))]
     public class ShowAmountsInItemTooltip
     {
-        public static void Postfix(Panel_ItemTooltip __instance, Table_ItemConsumable tableConsume)
+        [HarmonyPostfix]
+        [HarmonyPatch(nameof(Panel_ItemTooltip.SetItemTooltip), typeof(Table_ItemConsumable))]
+        public static void ConsumableTooltip(
+            Panel_ItemTooltip __instance,
+            Table_ItemConsumable tableConsume
+        )
         {
-            LAOPLUS.Log.LogInfo($"ShowAmountsInItemTooltip.Postfix: {tableConsume.ItemName}");
+            LAOPLUS.Log.LogInfo($"ConsumableTooltip.Postfix: {tableConsume.Key}");
             var amounts = SingleTon<DataManager>.Instance.GetItemConsumableStackCount(
                 tableConsume.Key
             );
+            // ラベルの扱いはWarehouseの表記と合わせると良さそう（右寄せ・サイズ変動）
+            __instance._lblEnchantLevel.overflowMethod = UILabel.Overflow.ResizeFreely;
+            __instance._lblEnchantLevel.text = $"x{amounts}";
+        }
+
+        [HarmonyPostfix]
+        [HarmonyPatch(nameof(Panel_ItemTooltip.SetItemTooltip), typeof(Table_ItemEquip))]
+        public static void ItemTooltip(Panel_ItemTooltip __instance, Table_ItemEquip tableEquip)
+        {
+
+            LAOPLUS.Log.LogInfo($"ItemTooltip.Postfix: {tableEquip.Key}");
+            var items = SingleTon<DataManager>.Instance._invenItemInfo;
+            var amounts2 = items.Select(x => x.Key).Where(x => x == tableEquip.Key).ToList();
+            // var amounts = items.Cast<System.Collections.Generic.List<ClientItemInfo>>();
+            var amounts = 0;
+            foreach (var item in items)
+            {
+                if (item.ItemKeyString == tableEquip.Key)
+                {
+                    amounts++;
+                }
+            }
+
             // ラベルの扱いはWarehouseの表記と合わせると良さそう（右寄せ・サイズ変動）
             __instance._lblEnchantLevel.overflowMethod = UILabel.Overflow.ResizeFreely;
             __instance._lblEnchantLevel.text = $"x{amounts}";
