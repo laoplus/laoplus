ef7146a353a30b342166cb04a2e74ad07c9767a3
Irie Aoi <eai@mizle.net>
2023-06-01 00:47:51 +0900

    プロジェクトフォルダの移動とテストプロジェクトの追加

---
+++ LAOPLUS.Tests/Tests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Xunit;
+
+namespace LAOPLUS.Tests
+{
+    public class Tests
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.True(true);
+        }
+    }
+}
